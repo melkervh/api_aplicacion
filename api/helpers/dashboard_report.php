bccf616344579277db1247dc0304b8fc31f4b87c
melkervh <jorgesecas@gmail.com>
2022-09-25 17:24:15 -0600

    a

--- api/helpers/dashboard_report.php
+++ api/helpers/dashboard_report.php
@@ -2,6 +2,7 @@
 require('../../helpers/database.php');
 require('../../helpers/validator.php');
 require('../../libraries/fpdf182/fpdf.php');
+header('Access-Control-Allow-Origin: *');
 /**
 *   Clase para definir las plantillas de los reportes del sitio privado. Para más información http://www.fpdf.org/
 */
